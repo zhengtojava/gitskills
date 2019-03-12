f55f5e68e74bb8349dbe6527e94126ce3e7ad711
zj <15145226631@163.com>
2019-03-12 13:41:00 +0800

    github 提交代码采用的是idea

---
+++ demo2/src/main/java/com/sproingboot/demo2/demo/SpringTigger.java
@@ -0,0 +1,11 @@
+package com.sproingboot.demo2.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringTigger
+{
+    public void testfunction(){
+        System.out.println("我可以的");
+    }
+}
